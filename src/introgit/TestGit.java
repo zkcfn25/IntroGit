283fb2f8bf036f73a620224de823a3e6c87e9dda
ztc3@students.ptcollege.edu <>
2018-10-22 15:30:32 -0400

    This is the first commit

---
+++ src/introgit/TestGit.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introgit;
+
+/**
+ *
+ * @author ztc3
+ */
+public class TestGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        cofini();
+    }
+    public static void cofini()
+    {
+        System.out.println("Zak Cofini");
+    }
+}
